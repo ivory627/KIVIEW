78cdb16e80915e50c34ab59bb09f02978ee535d5
SEUNGHYE CHOI <wooaoe68@gmail.com>
2020-02-16 04:21:52 +0900

    git ignore 수정

--- KIVIEW_develop/src/main/java/com/mvc/kiview/model/biz/NoticeBizImpl.java
+++ KIVIEW_develop/src/main/java/com/mvc/kiview/model/biz/NoticeBizImpl.java
@@ -10,39 +10,38 @@ import com.mvc.kiview.model.vo.FAQVo;
 import com.mvc.kiview.model.vo.NoticeVo;
 
 @Service
-public class NoticeBizImpl implements NoticeBiz{
+public class NoticeBizImpl implements NoticeBiz {
 
 	@Autowired
 	private NoticeDao n_dao;
-	
-	
+
 	@Override
 	public List<NoticeVo> noticeList() {
 		return n_dao.noticeList();
 	}
-	
+
 	@Override
 	public NoticeVo n_selectOne(int notice_no) {
-		
+
 		n_dao.notice_hitupdate(notice_no);
-		
+
 		return n_dao.n_selectOne(notice_no);
 	}
-	
-	
+
 	@Override
 	public int notice_insert(NoticeVo n_vo) {
 		return n_dao.notice_insert(n_vo);
 	}
+	
 
 	@Override
 	public int notice_update(NoticeVo n_vo) {
-		
+
 		n_dao.notice_hitminus(n_vo.getNotice_no());
-		
+
 		return n_dao.notice_update(n_vo);
 	}
-	
+
 	@Override
 	public int notice_hitupdate(int notice_no) {
 		return n_dao.notice_hitupdate(notice_no);
@@ -52,7 +51,6 @@ public class NoticeBizImpl implements NoticeBiz{
 	public int notice_delete(int notice_no) {
 		return n_dao.notice_delete(notice_no);
 	}
-	
 
 	@Override
 	public List<FAQVo> faqList() {
@@ -78,13 +76,5 @@ public class NoticeBizImpl implements NoticeBiz{
 	public int faq_delete(int faq_no) {
 		return 0;
 	}
-
-	
-
-	
-
-	
-
-	
 
 }
