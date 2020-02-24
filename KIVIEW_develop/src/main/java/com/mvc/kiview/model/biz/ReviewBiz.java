10896a9ee277f0a593bd1980eaaf4b505b741ff9
DEAR-SUNSET <heeseason1@naver.com>
2020-02-24 10:41:01 +0900

    리뷰 입력, 삭제, 수정(일부)
    수정 체크 중

--- KIVIEW_develop/src/main/java/com/mvc/kiview/model/biz/ReviewBiz.java
+++ KIVIEW_develop/src/main/java/com/mvc/kiview/model/biz/ReviewBiz.java
@@ -6,9 +6,9 @@ import com.mvc.kiview.model.vo.ReviewVo;
 
 public interface ReviewBiz {
 	public List<ReviewVo> reviewList(); //목록
-	public int reviewInsert(); //게시글 쓰기
-	public int reviewUpdate(); //게시글 수정
-	public int reviewDelete(); //게시글 삭제
+	public int reviewInsert(ReviewVo vo); //게시글 쓰기
+	public int reviewUpdate(ReviewVo vo); //게시글 수정
+	public int reviewDelete(int review_no); //게시글 삭제
 	public int reviewSearch(); //검색
 
 }
